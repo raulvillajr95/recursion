e1192da7b7d845632961451d84bf6fca2315cba5
Raul <raulvillajr95@gmail.com>
2023-02-23 18:47:01 -0800

    Create function that returns array of Fibonacci numbers

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log('first');
+// Create array of fibonacci numbers(plain loop)
+function fibs(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    if (arr.length < 2) {
+      arr.push(i);
+    } else {
+      arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+    }
+  }
+  return arr;
+}
+
+console.log(fibs(8));
